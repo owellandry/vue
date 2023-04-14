44f38d7823eb7f021d302d35bc04896a50310372
owell andry <81540116+owellandry@users.noreply.github.com>
2023-04-14 21:16:22 +0000

    Updates files

---
+++ campus_online/src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login-container">
+    <form class="login-form">
+      <h1 class="login-title">Iniciar sesión</h1>
+      <div class="login-inputs">
+        <input type="text" class="input" placeholder="Usuario" />
+        <input type="password" class="input" placeholder="Contraseña" />
+        <a href="#" class="forgot-password">Recuperar contraseña</a>
+        <button class="btn">Ingresar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #252934;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  padding: 40px;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+}
+
+.login-title {
+  font-family: "Helvetica Neue", sans-serif;
+  margin-bottom: 30px;
+  color: #fff;
+  text-align: center;
+  font-size: 28px;
+  font-weight: bold;
+  text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3);
+}
+
+.login-inputs {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.1);
+  color: #fff;
+  font-size: 16px;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+}
+
+.forgot-password {
+  align-self: center;
+  cursor: pointer;
+  margin-bottom: 20px;
+  color: #fff;
+  text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3);
+  font-size: 20px;
+  text-decoration: none;
+}
+
+.btn {
+  width: 100%;
+  padding: 10px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #fff;
+  color: #252934;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  transition: all 0.3s ease-in-out;
+}
+
+.btn:hover {
+  background-color: #252934;
+  color: #fff;
+  box-shadow: 0 8px 45px 0 rgba(31, 38, 135, 0.57);
+  cursor: pointer;
+}
+
+</style>
---
+++ campus_online/src/login/login.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="container">
+      <h1>Iniciar sesión</h1>
+      <form>
+        <div class="input-container">
+          <label for="username">Nombre de usuario:</label>
+          <input type="text" id="username" name="username" v-model="username" required />
+          <div class="input-background"></div>
+        </div>
+        <div class="input-container">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" name="password" v-model="password" required />
+          <div class="input-background"></div>
+        </div>
+        <div class="button-container">
+          <button type="submit" class="primary">Iniciar sesión</button>
+        </div>
+      </form>
+      <div class="links">
+        <a href="#">¿Olvidaste tu contraseña?</a>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    border-radius: 30px;
+    background-color: #f0f3f5;
+    box-shadow: 20px 20px 60px #c9d1d9, -20px -20px 60px #ffffff;
+  }
+  
+  .input-container {
+    position: relative;
+    margin: 1rem 0;
+  }
+  
+  label {
+    position: absolute;
+    top: -1.2rem;
+    left: 0.8rem;
+    font-size: 1rem;
+    color: #808080;
+  }
+  
+  input {
+    width: 100%;
+    height: 3rem;
+    padding: 0.5rem 0.8rem;
+    border: none;
+    outline: none;
+    border-radius: 15px;
+    background-color: #f0f3f5;
+  }
+  
+  .input-background {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 3rem;
+    background-color: #c9d1d9;
+    border-radius: 15px;
+    z-index: -1;
+  }
+  
+  .button-container {
+    margin-top: 2rem;
+  }
+  
+  button {
+    width: 100%;
+    height: 3rem;
+    border: none;
+    border-radius: 15px;
+    background-color: #5d5fe5;
+    color: #fff;
+    font-size: 1.2rem;
+    cursor: pointer;
+  }
+  
+  .primary {
+    background-color: #5d5fe5;
+  }
+  
+  .links {
+    margin-top: 1rem;
+  }
+  
+  a {
+    color: #808080;
+    font-size: 0.8rem;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+      };
+    },
+  };
+  </script>
+  
